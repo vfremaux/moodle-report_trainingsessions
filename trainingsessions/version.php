b5447fd2ddcef9d8b35568a0a920dde018afbe57
Valery Fremaux <valery.fremaux@gmail.com>
2012-06-09 18:21:54 +0200

    try version switch

--- trainingsessions/version.php
+++ trainingsessions/version.php
@@ -27,4 +27,4 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2012052100; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011110200; // Requires this Moodle version
-$plugin->component = 'report_trainingsessions'; // Full name of the plugin (used for diagnostics)
+

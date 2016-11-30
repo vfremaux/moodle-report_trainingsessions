b2b2123e77b8693c3f5b1033461945adb9ceb0ce
valery <valery.fremaux@gmail.com>
2016-11-30 22:54:18 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -26,12 +26,12 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016051700; // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2015050500; // Requires this Moodle version
-$plugin->component = 'report_trainingsessions'; // Full name of the plugin (used for diagnostics)
+$plugin->version   = 2016051700; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2015050500; // Requires this Moodle version.
+$plugin->component = 'report_trainingsessions'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.9.0 (build 2016051700)';
 $plugin->dependencies = array('block_use_stats' => '2016051700', 'auth_ticket' => '*');
 
-// Non Moodle attributes.
+// Non moodle attributes.
 $plugin->codeincrement = '2.9.0000';

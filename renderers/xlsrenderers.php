3343a271cd254f7c8e9374662f6638fd4d363b49
valery <valery.fremaux@gmail.com>
2017-01-18 18:18:58 +0100

    Fix XLS generation with additional columns

--- renderers/xlsrenderers.php
+++ renderers/xlsrenderers.php
@@ -594,6 +594,9 @@ function report_trainingsessions_print_allcourses_xls(&$worksheet, &$aggregate, 
  */
 function report_trainingsessions_print_rawline_xls(&$worksheet, $data, $dataformats, $row, &$xlsformats) {
 
+    print_object($dataformats);
+    print_object($xlsformats);
+
     for ($i = 0; $i < count($data); $i++) {
 
         if (!array_key_exists($dataformats[$i], $xlsformats)) {

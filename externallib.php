ba7e0a79cf80aad960817b325a8af1733e65edb5
valery <valery.fremaux@gmail.com>
2017-02-16 17:53:16 +0100

    Fix cronlib call to task shift function in pro version

--- externallib.php
+++ externallib.php
@@ -27,128 +27,114 @@ defined('MOODLE_INTERNAL') || die;
 
 class report_trainingsessions_external {
 
-    public function fetch_report($reporttype, $reportscope, $reportformat, $from, $to, $courseid = 0, $groupid = 0, $userid = 0) {
+    public static function validate_report_parameters($configparamdefs, $inputs) {
 
-        // Ensure report format is always lowercase.
-        $reportformat = strtolower($reportformat);
+        $status = self::validate_parameters($configparamdefs, $inputs);
 
-        if (!in_array($reportformat, array('csv', 'xls', 'pdf', 'json'))) {
-            $data = new StdClass();
-            $data->errorcode = 100;
-            return;
+        if (!in_array($inputs['reportformat'], array('csv', 'xls', 'pdf', 'json'))) {
+            throw new invalid_parameter_exception('Unsupported output document format');;
         }
 
-        if (!in_array($reportscope, array('allcourses', 'currentcourse'))) {
-            $data = new StdClass();
-            $data->errorcode = 200;
-            return;
+        if (!in_array($inputs['reportscope'], array('allcourses', 'currentcourse'))) {
+            throw new invalid_parameter_exception('Unsupported report scope');;
         }
 
-        $reporttypes = array('onefulluserpersheet', 'onefulluserperfile', 'oneuserperrow', 'alluserssessionsinglefile', 'sessions');
-        if (!in_array($reporttype, $reporttypes)) {
-            $data = new StdClass();
-            $data->errorcode = 300;
-            return;
+        $reportlayouts = array('onefulluserpersheet', 'onefulluserperfile', 'oneuserperrow', 'alluserssessionsinglefile', 'sessions');
+        if (!in_array($input['reportlayout'], $reportlayouts)) {
+            throw new invalid_parameter_exception('Unsupported report scope');;
         }
 
-        // Resolve the adequate document generator to call.
-        switch ($reportformat) {
-            case 'xls':
-                switch ($reportlayout) {
-                    case 'onefulluserpersheet':
-                        $reporttype = 'report';
-                        $range = 'user';
-                        $rangeid = $user->username;
-                        $uri = new moodle_url('/report/trainingsessions/tasks/groupxlsreportperuser_batch_task.php');
-                        break;
-
-                    case 'oneuserperrow':
-                        $reporttype = 'summary';
-                        $range = 'group';
-                        $rangeid = $groupid;
-                        $uri = new moodle_url('/report/trainingsessions/tasks/groupxlsreportsummary_batch_task.php');
-                        break;
-
-                    default:
-                        $reporttype = 'sessions';
-                        $range = 'user';
-                        $rangeid = $user->username;
-                        $uri = new moodle_url('/report/trainingsessions/tasks/userxlssessionsreport_batch_task.php');
-                        break;
+        switch ($inputs['courseidsource']) {
+            case 'id': {
+                if (!$DB->record_exists('course', array('id' => $inputs['courseid']))) {
+                    throw new invalid_parameter_exception('Course not found by id: '.$inputs['courseid']);
                 }
                 break;
+            }
 
-            case 'pdf':
-                switch ($reportlayout) {
-                    case 'onefulluserpersheet':
-                        $reporttype = 'peruser';
-                        $range = 'group';
-                        $rangeid = $groupid;
-                        break;
+            case 'shortname': {
+                if (!$course = $DB->get_record('course', array('shortname' => $inputs['courseid']))) {
+                    throw new invalid_parameter_exception('Course not found by shortname: '.$inputs['courseid']);
+                }
+                $status['courseid'] = $course->id;
+                break;
+            }
 
-                    case 'onefulluserperfile':
-                        $reporttype = 'peruser';
-                        $range = 'user';
-                        $rangeid = $user->username;
-                        break;
+            case 'idnumber': {
+                if (!$course = $DB->get_record('course', array('idnumber' => $inputs['courseid']))) {
+                    throw new invalid_parameter_exception('Course not found by idnumber: '.$inputs['courseid']);
+                }
+                $status['courseid'] = $course->id;
+                break;
+            }
+        }
 
-                    case 'oneuserperrow':
-                        $reporttype = 'summary';
-                        $range = 'group';
-                        $rangeid = $groupid;
-                        break;
-
-                    case 'allusersessionssinglefile':
-                        $reporttype = 'sessions';
-                        $range = 'group';
-                        $rangeid = $groupid;
-                        break;
-
-                    default:
-                        $reporttype = 'sessions';
-                        $range = 'user';
-                        $rangeid = $user->username;
+        switch ($inputs['groupidsource']) {
+            case 'id': {
+                if (!$DB->record_exists('groups', array('id' => $inputs['groupid']))) {
+                    throw new invalid_parameter_exception('Group not found by id: '.$inputs['groupid']);
                 }
                 break;
+            }
 
-            case 'json':
+            case 'name': {
+                if (!$group = $DB->get_record('groups', array('name' => $inputs['groupid']))) {
+                    throw new invalid_parameter_exception('Group not found by name: '.$inputs['groupid']);
+                }
+                $status['groupid'] = $group->id;
                 break;
+            }
+        }
 
-            default:
-                // Csv case.
-                switch ($reportlayout) {
-                    default:
-                        assert(1);
+        switch ($inputs['useridsource']) {
+            case 'id': {
+                if (!$DB->record_exists('user', array('id' => $inputs['userid']))) {
+                    throw new invalid_parameter_exception('User not found by id: '.$inputs['userid']);
+                }
+                break;
+            }
+
+            case 'username': {
+                if (preg_match('/^(.*)§(.*)$/', $inputs['userid'])) {
+                    list($username, $hostroot) = explode('§', $inputs['userid']);
+                    $hostid = $DB->get_field('mnet_host', 'id', array('wwwroot' => $hostroot));
+                } else {
+                    $hostid = $CFG->mnet_localhost_id;
+                }
+                if (!$user = $DB->get_record('user', array('username' => $inputs['userid'], 'mnethostid' => $hostid))) {
+                    throw new invalid_parameter_exception('User not found by username: '.$inputs['userid']);
+                }
+                $status['userid'] = $user->id;
+                break;
+            }
+
+            case 'idnumber': {
+                    if (!$user = $DB->get_record('user', array('idnumber' => $inputs['userid']))) {
+                        throw new invalid_parameter_exception('User not found by idnumber: '.$inputs['userid']);
+                    }
+                    $status['userid'] = $user->id;
+                    break;
+                }
+
+            case 'email': {
+                    if (!$user = $DB->get_record('user', array('email' => $inputs['userid']))) {
+                        throw new invalid_parameter_exception('User not found by email: '.$inputs['userid']);
+                    }
+                    $status['userid'] = $user->id;
+                    break;
                 }
         }
 
-        $uri = new moodle_url('/report/trainingsessions/tasks/'.$range.$reportformat.'report'.$reporttype.'_batch_task.php');
-        if ($reportformat != 'json') {
-            $filename = 'trainingsessions_'.$range.'_'.$rangeid.'_'.$reporttype.'_'.date('Y-M-d', time()).'.'.$reportformat;
-        } else {
-            $filename = '';
-        }
-
-        $timesession = time();
-
-        $result = new StdClass;
-        $result->filename = $filename;
-        if ($range == 'user') {
-            $result->file = report_trainingsessions_process_group_file($group, $id, $from, $to, $timesession, $uri, null, $reportscope);
-        } else {
-            $result->file = report_trainingsessions_process_user_file($group, $id, $from, $to, $timesession, $uri, null, $reportscope);
-        }
-
-        return json_encode($result);
+        return $status;
     }
 
-    public static function fetch_report_parameters() {
+    public static function get_report_url_parameters() {
 
         return new external_function_parameters (
             array(
-                '$reporttype' => new external_value(
+                '$reportlayout' => new external_value(
                         PARAM_ALPHA,
-                        'report type'),
+                        'Report layout'),
                 'reportscope' => new external_value(
                         PARAM_ALPHA,
                         'scope of data scanned for report'),
@@ -161,20 +147,59 @@ class report_trainingsessions_external {
                 'to' => new external_value(
                         PARAM_INT,
                         'period end timestamp'),
+                'courseidsource' => new external_value(
+                        PARAM_ALPHA,
+                        'The source for course id. Can be "id", "idnumber", "shortname"'),
                 'courseid' => new external_value(
-                        PARAM_INT,
+                        PARAM_TEXT,
                         'course target restriction (for group ranged reports)'),
+                'groupidsource' => new external_value(
+                        PARAM_ALPHA,
+                        'The source for group id. Can be "id" or "name"'),
                 'groupid' => new external_value(
-                        PARAM_INT,
+                        PARAM_TEXT,
                         'group target restriction (for group ranged reports)'),
+                'useridsource' => new external_value(
+                        PARAM_ALPHA,
+                        'The source for user id. Can be "id", "username", "idnumber" or "email"'),
                 'userid' => new external_value(
-                        PARAM_INT,
+                        PARAM_TEXT,
                         'user targetting restriction (for user range reports)'),
             )
         );
     }
 
-    public static function fetch_report_returns() {
-        return new external_value(PARAM_RAW, 'a document content in a single object');
+    public function get_report_url($reportlayout, $reportscope, $reportformat, $from, $to, $courseidsource, $courseid = 0,
+                               $groupidsource = '', $groupid = 0, $useridsource = '', $userid = 0) {
+        global $CFG;
+
+        // Ensure report format is always lowercase.
+        $reportformat = strtolower($reportformat);
+
+        // Validate parameters.
+        $parameters = array('reportlayout' => $reportlayout,
+            'reportscope' => $reportscope,
+            'reportformat' => $reportformat,
+            'from' => $from,
+            'to' => $to,
+            'courseidsource' => $courseidsource,
+            'courseid' => $courseid,
+            'groupidsource' => $groupidsource,
+            'groupid' => $groupid,
+            'useridsource' => $useridsource,
+            'userid' => $userid);
+        $params = self::validate_report_parameters(self::get_report_url_parameters(), $parameters);
+
+        if (report_trainingsessions_supports_feature('export/ws')) {
+            include_once($CFG->dirroot.'/report/trainingsessions/pro/locallib.php');
+            return report_trainingsessions_export_report($reportlayout, $reportscope, $reportformat, $from, $to,
+                                                         $params['courseid'], $params['groupid'], $params['userid']);
+        } else {
+            throw new moodle_exception('Feature not supported in this release.');
+        }
+    }
+
+    public static function get_report_url_returns() {
+        return new external_value(PARAM_URL, 'An Url to the file');
     }
 }

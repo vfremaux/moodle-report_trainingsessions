24a5dc36c602f2c5874cb0f6350822dce3784701
valery <valery.fremaux@gmail.com>
2017-07-31 19:19:29 +0200

    repair miscommiting from 3.0

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2017021600; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2015111000; // Requires this Moodle version.
+$plugin->requires  = 2016052300; // Requires this Moodle version.
 $plugin->component = 'report_trainingsessions'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.0.0 (build 2017021600)';
+$plugin->release = '3.1.0 (build 2017021600)';
 $plugin->dependencies = array('block_use_stats' => '2016051700', 'auth_ticket' => '2012060400');
 
 // Non moodle attributes.

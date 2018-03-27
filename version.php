c0fda9bfc6b2603c2d47b7846c59c6414db4bf9d
valery <valery.fremaux@gmail.com>
2018-03-27 12:08:05 +0200

    fix lib call

--- version.php
+++ version.php
@@ -34,5 +34,5 @@ $plugin->release = '3.2.0 (build 2018032400)';
 $plugin->dependencies = array('block_use_stats' => '2018032400', 'auth_ticket' => '2012060400');
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0010';
+$plugin->codeincrement = '3.2.0011';
 $plugin->privacy = 'dualrelease';

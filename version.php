87ff8d89ae42d79cc8f91ae91f79b99b8ed2dc62
valery <valery.fremaux@gmail.com>
2018-12-26 11:49:10 +0100

    Push V3.6 + travis

--- version.php
+++ version.php
@@ -27,12 +27,12 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2018032400; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018042700; // Requires this Moodle version.
+$plugin->requires  = 2018112800; // Requires this Moodle version.
 $plugin->component = 'report_trainingsessions'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.5.0 (build 2018032400)';
+$plugin->release = '3.6.0 (build 2018032400)';
 $plugin->dependencies = array('block_use_stats' => '2018032400', 'auth_ticket' => '2012060400');
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.5.0011';
+$plugin->codeincrement = '3.6.0011';
 $plugin->privacy = 'dualrelease';

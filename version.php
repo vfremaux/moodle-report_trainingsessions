ca6ff7eb66f3b3e54eefefddbe4acbda4ae39bf5
Valery Fremaux <valery.fremaux@gmail.com>
2021-02-10 20:20:11 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -26,13 +26,13 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2019112200; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020100500; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2019111200; // Requires this Moodle version.
 $plugin->component = 'report_trainingsessions'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.8.0 (build 2019111200)';
+$plugin->release = '3.8.0 (build 2020100500)';
 $plugin->dependencies = array('block_use_stats' => '2020031300', 'auth_ticket' => '2012060400');
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.8.0016';
+$plugin->codeincrement = '3.8.0019';
 $plugin->privacy = 'dualrelease';

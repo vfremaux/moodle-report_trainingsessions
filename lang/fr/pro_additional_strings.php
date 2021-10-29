2c4c1191b0a19a9a655d57aa4dee38d26c9066eb
Valery Fremaux <valery.fremaux@gmail.com>
2021-10-29 16:54:02 +0200

    Automated sync from SVN

--- lang/fr/pro_additional_strings.php
+++ lang/fr/pro_additional_strings.php
@@ -8,6 +8,9 @@ sous certaines conditions, afin de soutenir l\'effort de développement, amélio
 <p>Contactez un distributeur pour obtenir la version "Pro" et son support.</p>
 <p><a href="http://www.mylearningfactory.com/index.php/documentation/Distributeurs?lang=fr_utf8">Distributeurs MyLF</a></p>';
 
+// Caches.
+$string['cachedef_pro'] = 'Stocke des données spécifiques de la zone "pro"';
+
 require_once($CFG->dirroot.'/report/trainingsessions/lib.php'); // to get xx_supports_feature();
 if ('pro' == report_trainingsessions_supports_feature()) {
     include($CFG->dirroot.'/report/trainingsessions/pro/lang/fr/pro.php');
